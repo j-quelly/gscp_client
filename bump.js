8152cfae2cf3d2d09266eb093b083907246d6ca3
James Brouillet <james_brouillet@hotmail.com>
2018-09-17 11:23:21 -0700

    Updated Scrtip
    Updated Node Script.

--- bump.js
+++ bump.js
@@ -4,55 +4,41 @@
  * node /path/to/script versionNumber
  */
 const fs = require('fs');
+const encoding = 'utf-8';
 
 // File Location
-const mdFile = "./README.md";
-const packageFile = "./package.json";
+const updateVersions = [
+    mdFile =  {
+        "path" : "./README.md",
+        "pattern": /\d{1,}\.\d{1,}\.\d{1,}/
+    },
+    packageFile = {
+        "path" : "./package.json",
+        "pattern": /\d{1,}\.\d{1,}\.\d{1,}/
+    }
+]
 
 // Require a version to update
 if (process.argv[2] === undefined){
     throw new Error("Missing version string for update");
 }
 
-// Read MD file
-fs.readFile(mdFile, 'utf8' ,(err, data) => {
-    if (err){
-        throw new Error(err);
-    }
-
-    // String replacement
-    let newVersion = data.replace(/\# GSCP Client \d{1,}\.\d{1,}\.\d{1,}/, '# GSCP Client ' + process.argv[2]);
-
-    // Output
-    fs.writeFile(mdFile, newVersion, 'utf-8', (err) => {
+for (let i = 0; i < updateVersions.length; i++){
+    fs.readFile(updateVersions[i].path, encoding, (err , data) => {
         if (err){
-            throw new Error(err);
+            // Error
+            console.log(err);
+        } else {
+            // Replace String
+            let newVersion = data.replace(updateVersions[i].pattern, process.argv[2]);
+
+            // Output
+            fs.writeFile(updateVersions[i].path, newVersion, encoding, (err) => {
+                // Error
+                if (err){
+                    console.log(err);
+                }
+            });
         }
-
-        console.log("Version updated in md file");
     });
-    
-});
-
-// Package.JSON Update
-fs.readFile(packageFile, (err, data) => {
-    if (err){
-        throw new Error(err);
-    }
-
-    // Parse JSON
-    let parsedData = JSON.parse(data);
-    parsedData.version = process.argv[2];
-    let newData = JSON.stringify(parsedData);
-
-    // Write Update
-    fs.writeFile(packageFile, newData, (err) => {
-        if (err){
-            throw new Error(err);
-        }
-        console.log("Package Json file updated");
-    });
-});
-
-// Complete
-console.log("Version Update Complete");
+}
